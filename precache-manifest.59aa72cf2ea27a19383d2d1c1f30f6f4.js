873100ddc044f063dcb9c1271d2bb679457ead63
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-18 13:01:30 +0530

    Updates

---
+++ precache-manifest.59aa72cf2ea27a19383d2d1c1f30f6f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29778669d7a6efa3d1309a9a20675c49",
+    "url": "/Sidebar-Modal-React-Project/index.html"
+  },
+  {
+    "revision": "098ce945ad2d7fef0be4",
+    "url": "/Sidebar-Modal-React-Project/static/css/main.19d3afdf.chunk.css"
+  },
+  {
+    "revision": "776fb6bd60c017fa01e0",
+    "url": "/Sidebar-Modal-React-Project/static/js/2.64cb8197.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sidebar-Modal-React-Project/static/js/2.64cb8197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098ce945ad2d7fef0be4",
+    "url": "/Sidebar-Modal-React-Project/static/js/main.7d7356ff.chunk.js"
+  },
+  {
+    "revision": "1343734a9c0395fe8ab0",
+    "url": "/Sidebar-Modal-React-Project/static/js/runtime-main.bf4f1aab.js"
+  },
+  {
+    "revision": "0eb8b5ca7c0a2529dd3ec74e77726b1e",
+    "url": "/Sidebar-Modal-React-Project/static/media/logo.0eb8b5ca.svg"
+  }
+]);
